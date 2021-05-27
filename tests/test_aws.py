9482dc6175fa39344db311e303023e9c30616c5e
fushang318 <fushang318@gmail.com>
2021-05-27 11:29:48 +0800

    add sts upload_s3_assume_role

---
+++ tests/test_aws.py
@@ -0,0 +1,25 @@
+import pytest
+
+from app.common.models.icpdao.user import User
+from app.common.models.icpdao.user_github_token import UserGithubToken
+
+from .base import Base
+
+
+class TestAws(Base):
+ 
+    def test_sts_upload_s3_assume_role(self):
+        self.clear_db()
+
+        user = self.create_icpper_user('user')
+
+        # mentor 邀请 user1
+        res = self.client.get(
+            '/aws/sts/upload_s3_assume_role',
+            headers={'user_id': str(user.id)}
+        )
+        assert res.status_code == 200
+
+        assert res.json()['data']['access_key_id'] == 'AccessKeyId'
+        assert res.json()['data']['secret_access_key'] == 'SecretAccessKey'
+        assert res.json()['data']['session_token'] == 'SessionToken'
