1dc79011f96593f158faa2896c481c96806a5a32
bqx619 <bqx619@gmail.com>
2021-11-10 00:17:48 +0800

    fix: env settings

--- app/helpers/discord.py
+++ app/helpers/discord.py
@@ -1,12 +1,12 @@
 import requests
 
-from settings import ICPDAO_BOT_TOKEN
+from settings import ICPDAO_DISCORD_BOT_TOKEN
 
 def set_discord_role(guild_id, user_id, role_id) -> bool:
     url = f"https://discord.com/api/v8/guilds/{guild_id}/members/{user_id}/roles/{role_id}"
 
     headers = {
-        "Authorization": "Bot " + ICPDAO_BOT_TOKEN
+        "Authorization": "Bot " + ICPDAO_DISCORD_BOT_TOKEN
     }
     r = requests.put(url, headers=headers)
     if r.status_code == 200:
